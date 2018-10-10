b6ed228241fb8acf5e77114d6e88407c7cf3c909
mikkiGuo <mikkiguo29@yahoo.com>
2018-10-10 16:20:20 -0500

    created floating button layout, created two flavors, manager and developer

--- app/src/main/java/com/example/mikki/projectmanagement/MainActivity.kt
+++ app/src/main/java/com/example/mikki/projectmanagement/MainActivity.kt
@@ -2,11 +2,19 @@ package com.example.mikki.projectmanagement
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.Snackbar
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.floating_button.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+        }
     }
 }
