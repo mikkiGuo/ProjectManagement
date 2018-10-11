5baeb0363826c829f0f1c66e72dfb9c6fb357bdb
NinnYu <nychin92@gmail.com>
2018-10-10 23:31:11 -0500

    modified bottom navigation

--- app/src/main/java/com/example/mikki/projectmanagement/CreateTaskActivity.kt
+++ app/src/main/java/com/example/mikki/projectmanagement/CreateTaskActivity.kt
@@ -2,11 +2,23 @@ package com.example.mikki.projectmanagement
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.Snackbar
+import kotlinx.android.synthetic.main.bottom_navigation.*
+import kotlinx.android.synthetic.main.floating_button.*
 
 class CreateTaskActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_task)
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+        }
+
+        /*bottom_navigation.setOnNavigationItemSelectedListener {
+
+        }*/
     }
 }
